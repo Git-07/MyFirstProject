3713c872326072d1dcad5880884c89c86e80c002
Git-07 <43235173+Git-07@users.noreply.github.com>
2018-09-13 15:39:45 +0530

    Select_Only_One_Character_Among_The_Two_Repeated_Characters

---
+++ Two_Repeated_Char.java
@@ -0,0 +1,38 @@
+package Learning_Pattern_Package;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Two_Repeated_Char {
+	// among the repeated characters remove one from the String
+	public static void main(String[] args) {
+		try {
+			// String l = "aeeeecec";
+			Scanner scan = new Scanner(System.in);
+			String s = scan.next();
+			int count = 1;
+			int counter = 0;
+			int i = 0;
+			char[] ch = s.toCharArray();
+			while (count < ch.length) {
+
+				if (ch[count] == ch[count - 1]) {
+					counter++;
+				} else if (ch[count - 1] != ' ') {
+					System.out.print(ch[count - 1]);
+				}
+				if (counter == 1) {
+					System.out.print(ch[count]);
+					ch[count] = ' ';
+					counter = 0;
+				}
+				count++;
+			}
+			if (ch[ch.length - 1] != ch[ch.length - 2]) {
+				System.out.print(ch[ch.length - 1]);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
