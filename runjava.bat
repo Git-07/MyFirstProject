6cce0c59f423e43d10c55a58d0ac6bb3c33b7bf2
Git-07 <43235173+Git-07@users.noreply.github.com>
2018-12-29 13:37:46 +0530

    Update runjava.bat
    commit

--- runjava.bat
+++ runjava.bat
@@ -1,5 +1,7 @@
-set projectPath=https://github.com/Git-07/MyRepo.git
+git clone https://github.com/Git-07/MyRepo.git Local_Repo
+set projectPath=C:\Users\mohit\Local_Repo
 cd\
 cd %projectPath%
-set classpath=%projectPath/Get_The_Minimum_Index.class;
-java Get_The_Minimum_Index
+set classpath=%projectPath%
+javac -d . Get_The_Minimum_Index.java
+java Learning_Pattern_Package.Get_The_Minimum_Index
