f05b175843cdefed080a9ecaa97cdec40b2de07e
Git-07 <43235173+Git-07@users.noreply.github.com>
2018-12-29 11:21:01 +0530

    Update runjava.bat
    change

--- runjava.bat
+++ runjava.bat
@@ -1,5 +1,5 @@
 set projectPath=https://github.com/Git-07/MyRepo.git
 cd\
 cd %projectPath%
-set classpath=%projectPath%;%projectPath%;
+set classpath=%projectPath/Get_The_Minimum_Index.class;
 java Get_The_Minimum_Index
